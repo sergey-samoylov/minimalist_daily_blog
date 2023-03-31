1150ae06f0b099b0a0995275220458c7f50f5b2f
Sergey Samoylov <110048435+sergey-samoylov@users.noreply.github.com>
2023-03-31 19:05:55 +0300

    Update settings.py

--- ypnews/ypnews/settings.py
+++ ypnews/ypnews/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-py)g1x4()o&^8q2)&&mxs_qa**@!yxx(%agwp@%z*+j(ss@xd5'
+SECRET_KEY = 'Your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
